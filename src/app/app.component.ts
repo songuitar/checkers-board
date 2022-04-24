c4ade7f7ef8a011121b6f88b3c57aee3ea35f50b
Alexey <a.bezborodov@skyeng.ru>
2022-04-24 08:04:41 +0300

    board state validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,24 @@
 import {Component, OnInit} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
-import {catchError, map, Observable, of, switchMapTo, timer} from "rxjs";
+import {
+  BehaviorSubject,
+  catchError,
+  distinctUntilChanged,
+  filter,
+  map,
+  Observable,
+  of,
+  switchMap,
+  switchMapTo,
+  tap, throwError,
+  timer, withLatestFrom
+} from "rxjs";
+
+enum Figure {
+  empty = 0,
+  black = 1,
+  white = 2
+}
 
 @Component({
   selector: 'app-root',
@@ -21,24 +39,39 @@ export class AppComponent implements OnInit {
   ]
 
   // @ts-ignore
-  boardState$: Observable<Array<Number[]>>
+  boardState$: Observable<Number[][]>
+  validationError$ = new BehaviorSubject<string | null>(null);
 
-  readonly blackFigure = 1;
-  readonly whiteFigure = 2;
+  readonly Figure = Figure;
 
   constructor(private http: HttpClient) {
   }
 
   ngOnInit(): void {
     this.boardState$ = timer(0, 1000).pipe(
-      switchMapTo(this.http.get<{board: Array<Number[]>}>('http://localhost:3000')),
-      catchError(error => of({board: this.defaultBoardState})),
-      map((res) => res.board)
+      switchMapTo(this.http.get<{ board: Number[][] }>('http://localhost:3000')),
+      map((res) => res.board),
+    //  distinctUntilChanged(((previous, current) => JSON.stringify(previous) === JSON.stringify(current))),
+      switchMap(state => {
+        if (!this.isStateValid(state)) {
+          this.validationError$.next('board state is not valid')
+          return of(state)
+        }
+
+        this.validationError$.next(null)
+        return of(state)
+      }),
     )
   }
 
   isBlackCell(rowIndex: number, cellIndex: number): boolean {
     const offset = rowIndex % 2 === 0 ? 0 : 1;
     return (cellIndex + offset) % 2 !== 0
+  }
+
+  private isStateValid(state: Number[][]): boolean {
+    return state.filter((row, rowIndex) =>
+      row.filter((cell, cellIndex) =>
+        !this.isBlackCell(rowIndex, cellIndex) && cell !== Figure.empty).length > 0).length === 0
   }
 }
