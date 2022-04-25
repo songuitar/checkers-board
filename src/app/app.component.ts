b8479518671f15235d014742785bdad8bc950564
Alexey <a.bezborodov@skyeng.ru>
2022-04-25 22:17:50 +0300

    fixed move validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import {
   filter,
   map,
   Observable,
-  of,
+  of, pairwise, startWith,
   switchMap,
   switchMapTo,
   tap, throwError,
@@ -27,39 +27,36 @@ enum Figure {
 })
 export class AppComponent implements OnInit {
   title = 'chess-board';
-  defaultBoardState = [
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-  ]
-
   // @ts-ignore
   boardState$: Observable<Number[][]>
   validationError$ = new BehaviorSubject<string | null>(null);
 
   readonly Figure = Figure;
+  readonly fullBoardFigureAmount = 24;
 
   constructor(private http: HttpClient) {
   }
 
   ngOnInit(): void {
-    this.boardState$ = timer(0, 1000).pipe(
+    this.boardState$ = timer(0, 3000).pipe(
       switchMapTo(this.http.get<{ board: Number[][] }>('http://localhost:3000')),
       map((res) => res.board),
+      startWith([[]]),
       distinctUntilChanged(((previous, current) => JSON.stringify(previous) === JSON.stringify(current))),
-      switchMap(state => {
-        if (!this.isStateValid(state)) {
+      pairwise(),
+      switchMap(([prev, curr]) => {
+        if (!this.isStateValid(curr)) {
           this.validationError$.next('board state is not valid')
-          return of(state)
+          return of(curr)
+        }
+
+        if (!this.isMoveValid(prev, curr)) {
+          this.validationError$.next('last move is not valid')
+          return of(curr)
         }
 
         this.validationError$.next(null)
-        return of(state)
+        return of(curr)
       }),
     )
   }
@@ -70,8 +67,18 @@ export class AppComponent implements OnInit {
   }
 
   private isStateValid(state: Number[][]): boolean {
-    return state.filter((row, rowIndex) =>
+    return this.stateSum(state) <= this.fullBoardFigureAmount && state.filter((row, rowIndex) =>
       row.filter((cell, cellIndex) =>
         !this.isBlackCell(rowIndex, cellIndex) && cell !== Figure.empty).length > 0).length === 0
+  }
+
+  private isMoveValid(prevState: Number[][], currState: Number[][]): boolean {
+    const prevSum = this.stateSum(prevState)
+    return prevSum ===0 || prevSum >= this.stateSum(currState);
+  }
+
+  private stateSum(state: Number[][]): number {
+    // @ts-ignore
+    return state.flat().reduce((b, a) => b + a, 0);
   }
 }
